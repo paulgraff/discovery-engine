e604ff93393137ea6b2de63e191c67f8da803899
Jamish <jamish@huskers.unl.edu>
2013-03-03 13:55:49 -0600

    Merge branch 'master' of https://github.com/paulgraff/discovery-engine
    Stuff.
    
    Conflicts:
    	js/main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ $(function() {
 		});
 	});
 });
+
+$('#deItem').click() {
+
+}
+
+var DiscoveryEngine = {
+	data: {},
+	template: '<li>{0}</li>'
+	reload: function() {
+		result = [];
+		// put in the api call to flickr here
+		return result;
+	},
+}
+
+ /* Protype additions */
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};

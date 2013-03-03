368f400d487e14ffb6cc11cfcbf83e23151bf250
Paul Graff <paulgraff@test1.mindmixer.net>
2013-03-03 14:21:35 -0600

    More stuffs

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 "use strict";
-$(function() {
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-	{
-		//tags: "mount rainier",
-		tagmode: "any",
-		format: "json"
-	},
-	function(data) {
-		$.each(data.items, function(i,item){
-			var html = "<img class='tile'/>";
-			$(html).attr("src", item.media.m).appendTo("#images");
-		});
+
+$(document).ready(function() {
+	$('#images').click(function() {
+		DiscoveryEngine.reload();
 	});
 });
 
-$('#deItem').click() {
-
-}
-
 var DiscoveryEngine = {
 	data: {},
-	template: '<li>{0}</li>'
+	template: '<img clas="tile" src="{0}"/>',
 	reload: function() {
-		result = [];
-		// put in the api call to flickr here
-		return result;
+		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+			{
+				//tags: "mount rainier",
+				tagmode: "any",
+				format: "json"
+			},
+			function(data) {
+				DiscoveryEngine.update(data);
+			});
 	},
-}
+	update: function(data) {
+		var body = '';
+		$.each(data.items, function(i,item){
+			body += DiscoveryEngine.template.format(item.media.m);
+		});
+		$('#images').html(body);
+	}
+};
+
+DiscoveryEngine.reload();
 
  /* Protype additions */
 String.prototype.format = function() {

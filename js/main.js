faa63c77a6f043ba55e2f35c48480d97b7d475f2
Jamish <jamish@huskers.unl.edu>
2013-03-03 13:52:40 -0600

    added index and css

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 "use strict";
-
+$(function() {
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+	{
+		//tags: "mount rainier",
+		tagmode: "any",
+		format: "json"
+	},
+	function(data) {
+		$.each(data.items, function(i,item){
+			var html = "<img class='tile'/>";
+			$(html).attr("src", item.media.m).appendTo("#images");
+		});
+	});
+});

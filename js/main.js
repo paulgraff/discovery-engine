f55cd8e241e93931a5fa26a4fc6ebdd8ea3325e9
Paul Graff <paulgraff@test1.mindmixer.net>
2013-03-04 11:06:03 -0600

    Added capty-- totes jank though so leaving it out

--- js/main.js
+++ js/main.js
@@ -10,7 +10,11 @@ $(window).resize(function() {
 
 var DiscoveryEngine = {
 	data: {},
-	template: '<div class="tile"><img src="{0}"/></div>',
+	template: '<div class="tile">' +
+				'<img src="{0}" alt="Something you want to buy - $590"/>' +
+				'<h2>A Movie in the Park:<br />Kung Fu Panda</h2>' +
+				'<a>' +
+				'</div>',
 	reload: function() {
 		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
 			{
@@ -30,7 +34,6 @@ var DiscoveryEngine = {
 
 		$('#images').html(body);
 		DiscoveryEngine.resizeWindow();
-
 	},
 	resizeWindow: function() {
 		var winHeight = $(window).height();
@@ -42,6 +45,7 @@ var DiscoveryEngine = {
 		   width : tileWidth,
 		   height : tileHeight
 		});
+		// $('.tile').capty();
 	}
 };
 
@@ -58,16 +62,16 @@ String.prototype.format = function() {
   });
 };
 
-$(document).on('click', '.tile', function () {
-	    $('.tile').flippy({
-		content:"Hi !",
-		direction:"RIGHT",
-		duration:"750",
-		onStart:function(){
-			//alert("Let's flip");
-		},
-		onFinish:function(){
-			//alert("ok, it's flipped :)");
-		}
-	});
-});
+// $(document).on('click', '.tile', function () {
+// 	    $('.tile').flippy({
+// 		content:"Hi !",
+// 		direction:"RIGHT",
+// 		duration:"750",
+// 		onStart:function(){
+// 			//alert("Let's flip");
+// 		},
+// 		onFinish:function(){
+// 			//alert("ok, it's flipped :)");
+// 		}
+// 	});
+// });
